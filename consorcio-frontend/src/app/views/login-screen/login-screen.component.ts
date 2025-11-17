81a5fae06e2d988ce0e33c60fd913814e3a69257
Augusto França <augustofranca0701@gmail.com>
2025-11-17 19:39:17 -0300

    Animação removida

--- consorcio-frontend/src/app/views/login-screen/login-screen.component.ts
+++ consorcio-frontend/src/app/views/login-screen/login-screen.component.ts
@@ -8,7 +8,6 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 import {merge} from 'rxjs';
 import { RouterOutlet, RouterLink, RouterLinkActive, Router } from '@angular/router';
-import { gsap } from 'gsap';
 import { CommonModule } from '@angular/common';
 import { ApiService } from '../../services/api.service';
 import { User } from '../../../models/User/user.model';
@@ -30,9 +29,8 @@ export class LoginScreenComponent implements OnInit {
   password = new FormControl('', [Validators.required]);
   errorMessage = '';
 
-  constructor(private visibilityService: VisibilityService, private apiService: ApiService, 
+  constructor(private visibilityService: VisibilityService, private apiService: ApiService,
     private snackBar: MatSnackBar, private userService: UserService, private router: Router) {
-    this.checkAnimationExecuted();
     this.visibilityService.setShowComponent(false);
     merge(this.email.statusChanges, this.email.valueChanges)
       .pipe(takeUntilDestroyed())
@@ -64,14 +62,7 @@ export class LoginScreenComponent implements OnInit {
 }
 
 
-  checkAnimationExecuted(): void {
-    if (!localStorage.getItem('animationExecuted')) {
-      this.animateCounters();
-      this.animateDigits();
-      this.animateLoader();
-      this.animateLoadingScreen();
-    }
-  }
+
 
   updateErrorMessage() {
     if (this.email.hasError('required')) {
@@ -84,106 +75,7 @@ export class LoginScreenComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.initAnimations(); // Inicializar animações
+
   }
 
-
-  private initAnimations(): void {
-    // Inicializar animações GSAP
-    this.animateCounters();
-    this.animateDigits();
-    this.animateLoader();
-    this.animateLoadingScreen();
-  }
-
-  private animateCounters(): void {
-    const counter3 = document.querySelector(".counter-3");
-
-    if (counter3) {
-      for (let i = 0; i < 2; i++) {
-        for (let j = 0; j < 10; j++) {
-          const div = document.createElement("div");
-          div.className = "num";
-          div.textContent = j.toString();
-          counter3.appendChild(div);
-        }
-      }
-      const finalDiv = document.createElement("div");
-      finalDiv.className = "num";
-      finalDiv.textContent = "0";
-      counter3.appendChild(finalDiv);
-
-      this.animate(counter3, 5);
-      this.animate(document.querySelector(".counter-4"), 2);
-      this.animate(document.querySelector(".counter-2"), 6);
-      this.animate(document.querySelector(".counter-1"), 2, 4);
-    }
-  }
-
-  private animate(counter: Element | null, duration: number, delay: number = 0): void {
-    if (counter) {
-      const numHeight = (counter.querySelector(".num") as HTMLElement).clientHeight;
-      const totalDistance = (counter.querySelectorAll(".num").length - 1) * numHeight;
-      gsap.to(counter, {
-        y: -totalDistance,
-        duration: duration,
-        delay: delay,
-        ease: "power2.inOut",
-      });
-    }
-  }
-
-  private animateDigits(): void {
-    gsap.to(".digit", {
-      top: "-150px",
-      stagger: {
-        amount: 0.25,
-      },
-      delay: 6,
-      duration: 1,
-      ease: "power4.inOut",
-    });
-  }
-
-  private animateLoader(): void {
-    gsap.from(".loader-1", {
-      width: 0,
-      duration: 6,
-      ease: "power2.inOut",
-    });
-
-    gsap.to(".loader", {
-      background: "none",
-      delay: 6,
-      duration: 0.1,
-    });
-
-    gsap.to(".loader-1", {
-      rotate: 90,
-      duration: 0.5,
-      delay: 6,
-    });
-
-    gsap.to(".loader", {
-      scale: 500,
-      duration: 1,
-      delay: 7,
-      ease: "power2.inOut",
-    });
-
-    gsap.to(".loader", {
-      duration: 1,
-      delay: 7,
-      ease: "power2.inOut",
-    });
-  }
-
-  private animateLoadingScreen(): void {
-    gsap.to(".loading-screen", {
-      opacity: 0,
-      duration: 0.5,
-      delay: 7.5,
-      ease: "power1.inOut",
-    });
-  }
 }
